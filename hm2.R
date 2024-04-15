195a88f9f5391e27b8435d5c41b25f0fcf01fc6c
Bogdan Karpovich <karpovich.bv@phystech.edu>
2024-04-15 15:37:01 +0300

    hm2 done

--- hm2.R
+++ hm2.R
@@ -1,23 +1,29 @@
 # mtcars
 head(mtcars)
 mtcars[c('qsec')]
-library(ggplot2)
-
-# Загрузка датасета mtcars
-data(mtcars)
+mtcars[c('mpg')]
+class(mtcars$mpg)
 
 # plot hist
 hist(mtcars$qsec, border="green", col = "steelblue", main="Заголовок Гистограммы", sub="Подзаголовок",
      xlab = "Ускорение", ylab = "Частота", cex.lab = 2,cex.axis = 2)
 
-# Compute the density data
-dens <- density(mtcars$qsec)
 # plot density
+dens <- density(mtcars$qsec)
+
 plot(dens,
      frame = FALSE,
      col = "steelblue",
      main = "Density plot of qsec")
 polygon(dens, col = "steelblue")
+
+# scatter plot
+plot(mtcars$disp, mtcars$mpg, pch = mtcars$cyl, cex = 2, col = mtcars$cyl, ylab="mpg",xlab="disp")
+
+# boxplot
+boxplot(mpg ~ cyl, data = mtcars,las=2)
+stripchart(mtcars$mpg ~ mtcars$cyl, vertical = TRUE, method = "jitter",
+           pch = 19, add = TRUE, col = 1:length(levels(chickwts$feed)))
 
 
 
