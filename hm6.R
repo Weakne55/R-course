3aebbd67ec4777851a215e12880c6190292bb728
Bogdan Karpovich <72615087+Weakne55@users.noreply.github.com>
2024-05-16 18:42:44 +0300

    RStudio bug
    RStudio не смог нормально закоммитить код, заметил это только сейчас.

--- hm6.R
+++ hm6.R
@@ -7,12 +7,9 @@
   library(pheatmap)
   library(ggsignif)
   library(ggfortify)
-  
+
   setwd('Homeworks')
 }
-
-data("ToothGrowth") 
-
 
 gse <- getGEO("GSE149507", GSEMatrix = TRUE)
 gse <- gse[[1]]
@@ -20,14 +17,21 @@ sampleinfo <- pData(gse)
 sampleinfo
 
 data <- data.frame(exprs(gse))
-head(data)
 
 boxplot(data, main='Raw counts distribution', las=2) # как видим данные нормализованы
 
 newdata <- as.data.frame(t(data))
 
 newdata$gender <- sampleinfo$'gender:ch1'
-newdata$tissue <- sampleinfo$'tissue:ch1'
 
-boxplot( ~ gender, data = newdata)
 
+newdata %>%
+  pivot_longer(cols = -gender, names_to = "genes") %>%
+  filter(genes %in% c("9_at", "999_at")) %>%
+  ggplot(aes(x = gender, y = value)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(aes(color = genes), width = 0.2) +
+  theme_minimal() +
+  facet_wrap(~ genes)
+
+boxplot('9_at' ~ gender, data = newdata)
